442569d97d5364f7d7c3ac0188c7a4b13db56948
Professor Breaker <jbreak1@ju.edu>
2025-10-02 10:46:35 -0400

    Initial Commit

---
+++ TestingBugsTests/TestingBugsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingBugsTests.swift
+//  TestingBugsTests
+//
+//  Created by P.Breaker on 10/2/25.
+//
+
+import Testing
+@testable import TestingBugs
+
+struct TestingBugsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
